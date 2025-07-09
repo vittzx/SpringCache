a95f36ce27ded8ebdee96f08a28dff773b1e0702
Vitor cosso <125705086+vittzx@users.noreply.github.com>
2025-07-09 19:26:43 -0300

    first commit

---
+++ src/main/java/com/springcache/cosso/CossoApplication.java
@@ -0,0 +1,13 @@
+package com.springcache.cosso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CossoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CossoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcache/cosso/CossoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcache.cosso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CossoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
